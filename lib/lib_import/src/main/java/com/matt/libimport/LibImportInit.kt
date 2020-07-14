cbc526eedd3c2a6ab2ed7aa4a1b8102e04c71db8
wgyscsf <wgyscsf@163.com>
2020-07-14 11:36:17 +0800

    完善基类、基类库

---
+++ lib/lib_import/src/main/java/com/matt/libimport/LibImportInit.kt
@@ -0,0 +1,17 @@
+package com.matt.libimport
+
+import android.app.Application
+import com.blankj.utilcode.util.Utils
+
+/**
+ * ============================================================
+ * 作 者 :    wgyscsf@163.com
+ * 创建日期 ：2020/7/14 11:22 AM
+ * 描 述 ：
+ * ============================================================
+ **/
+object LibImportInit {
+    fun init(application: Application) {
+        Utils.init(application)
+    }
+}
